df43467ed8d587258fa08e462187d72ecb22c3dc
John Fuquene <jfuquene@email.wm.edu>
2021-02-28 23:20:03 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
+
+let taskForm = document.getElementById('create-task-form')
+let taskList = document.querySelector('#task')
+
+taskForm.addEventListener('submit', function(e){
+  e.preventDefault()
+  
+  const newTask = document.getElementById("new-task-description").value
+
+
+  const taskItem = document.createElement("li")
+  taskItem.innerText = newTask
+  
+  taskList.append(taskItem)
+
+  taskForm.reset()
+})
+
